1c04ee21f41cda997c9dacb70654347d154d0e27
pnc-nz <10773075+pnc-nz@users.noreply.github.com>
2025-05-17 01:28:19 +0300

    Cors Toggle, Dark Mode Toggle, Developer/Local Mode Toggle, Support for '1x {Card}' pattern, Refactor Process Deck Button Handler and associated  function

--- script.js
+++ script.js
@@ -107,8 +107,13 @@ function getDeckIdFromUrl(deckUrl) {
 // Function to get deck data by deckId
 async function getDeckById(deckId, corsEnabled = True) {
   let original_url = `https://archidekt.com/api/decks/${deckId}/`;
-  // const url = "https://corsproxy.io/?" + original_url;
-  const url = "https://corsproxy.io/?" + encodeURIComponent(original_url);
+
+  // Config: CORS Toggle
+  if (corsEnabled) {
+    console.log('[DEBUG]: CORS Enabled')
+    const url = "https://corsproxy.io/?" + encodeURIComponent(original_url);
+  }
+
   if (deckId == -1) {
     url = "banlist_sample.json";
   }
@@ -163,7 +168,7 @@ function getCardNamesFromArchidektDeck(deckData, prepend_quantity = false) {
 function isValidExportFmt(input) {
   // Check if the input is a deck format
   // (e.g., cards listed by `{x} {name} {a} {b}` format, where x is a number followed by n space - separated strings)
-  const exportFormatPattern = /^\d+\s+.+$/;
+  const exportFormatPattern = /^\d+\s*x?\s+.+$/i;
   const lines = input.split("\n");
 
   // Check each line to see if it follows the card format
@@ -204,32 +209,59 @@ function isValidInput(input) {
 async function processDeck(deck) {
   // Config - General
   const disableCorsProxy = document.getElementById("disableCorsProxy").checked;
-  const enableDarkMode = document.getElementById("enableDarkMode").checked;
-  
+  const enableDeveloperMode = document.getElementById("enableDeveloperMode").checked;
+
   // Config - Deck & Cards
   const removeBannedCards = document.getElementById("removeBannedCards").checked;
   const removeBasicLands = document.getElementById("removeBasicLands").checked;
   const removeNonBasicLands = document.getElementById("removeNonBasicLands").checked;
 
-  let cardsToRemove = [];
-  // Handle banned deck(s)
-  if (removeBannedCards) {
-    const d = await getDeckById(bannedCardsDeckId, disableCorsProxy);
-    // const d = await getDeckById(-1);
-    const cards = getCardNamesFromArchidektDeck(d);
-    cardsToRemove.push(...cards);
+  // Validation: Input
+  if (!isValidInput(deck)) {
+    alert("Please enter a valid deck format or a valid Archidekt Deck URL. Your deck must be set to 'public'!");
+    return;
   }
-  if (removeBasicLands) {
-    const d = await getDeckById(basicLandsDeckId, disableCorsProxy);
-    // const d = await getDeckById(-2);
-    const cards = getCardNamesFromArchidektDeck(d);
+
+  // Validation: Archidekt URLs
+  if (isValidArchidektUrlFmt(deck)) {
+    const deckId = getDeckIdFromUrl(deck);
+    const inputDeck = await getDeckById(deckId, disableCorsProxy);
+    const cardList = getCardNamesFromArchidektDeck(inputDeck, true);
+    deck = cardList.join("\n");
+    document.getElementById("decklist").value = deck;
+  }
+
+  let cardsToRemove = [];
+
+  // Config: Developer Mode Toggle
+  if (enableDeveloperMode) {
+    bannedCardsDeckId = -1;
+    basicLandsDeckId = -2;
+    nonBasicLandsDeckId = -3;
+  } else {
+    basicLandsDeckId = "9637985";
+    nonBasicLandsDeckId = "7966401";
+    bannedCardsDeckId = "7853311";
+  }
+
+  // Remove: Banned Playgroup Cards
+  if (removeBannedCards) {
+    const bannedCardDeck = await getDeckById(bannedCardsDeckId, disableCorsProxy);
+    const cards = getCardNamesFromArchidektDeck(bannedCardDeck);
     cardsToRemove.push(...cards);
   }
 
+  // Remove: Basic Lands
+  if (removeBasicLands) {
+    const basicLandsDeck = await getDeckById(basicLandsDeckId, disableCorsProxy);
+    const cards = getCardNamesFromArchidektDeck(basicLandsDeck);
+    cardsToRemove.push(...cards);
+  }
+
+  // Remove: Non-Basic
   if (removeNonBasicLands) {
-    const d = await getDeckById(nonBasicLandsDeckId, disableCorsProxy);
-    // const d = await getDeckById(-3);
-    const cards = getCardNamesFromArchidektDeck(d);
+    const nonBasicLandsDeck = await getDeckById(nonBasicLandsDeckId, disableCorsProxy);
+    const cards = getCardNamesFromArchidektDeck(nonBasicLandsDeck);
     cardsToRemove.push(...cards);
   }
 
@@ -305,19 +337,7 @@ async function processDeck(deck) {
 
 /* BUTTON - PROCESS DECK */
 document.getElementById("processDeck").addEventListener("click", async () => {
-  let decklist = document.getElementById("decklist").value;
-  if (!isValidInput(decklist)) {
-    alert("Please enter a valid deck format or a valid Archidekt Deck URL. Your deck must be set to 'public'!");
-    return;
-  } else {
-    if (isValidArchidektUrlFmt(decklist)) {
-      // TODO: This needs to be refactored to modularly handle CORS Proxy Settings
-      input_deck = await getDeckById(getDeckIdFromUrl(decklist), disableCorsProxy);
-      decklist = getCardNamesFromArchidektDeck(input_deck, true).join("\n");
-      document.getElementById("decklist").value = decklist;
-    }
-    await processDeck(decklist);
-  }
+  await processDeck(document.getElementById("decklist").value);
 });
 
 /* BUTTON - RESET DECK */
@@ -326,3 +346,7 @@ document.getElementById("resetDeck").addEventListener("click", () => {
   document.getElementById("cards-include").textContent = "";
   document.getElementById("cards-remove").textContent = "";
 });
+
+// TODO - Implement
+/* BUTTON - DARK MODE/LIGHT MODE */
+document.getElementById("enableDarkMode").addEventListener("click", () => {});
